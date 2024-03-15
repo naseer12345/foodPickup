961e04e055591f94284efbb7cb66f2df7c6397a6
naseer12345 <109208526+naseer12345@users.noreply.github.com>
2024-03-15 17:18:23 +0000

    deployed to firebase functions

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,8 @@ const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 const Donations = require('./schemas/donationSchema')
-app.use(express.json()); // Middleware to parse JSON request bodies
+const functions = require("firebase-functions")
+app.use(express.json()); 
 app.use(cors());
 app.listen(3000, () => {
     console.log("server is running at port 3000");
@@ -13,7 +14,7 @@ app.listen(3000, () => {
 
 
 
-mongoose.connect('mongodb://localhost:27017/foodPick', {
+mongoose.connect('mongodb+srv://imaginaryworld15:GlfKlZvQ9NLI39EJ@cluster0.txcnlvz.mongodb.net/', {
   useNewUrlParser: true,
 })
 .then(() => console.log('MongoDB Connected'))
@@ -85,3 +86,5 @@ async function getDonor(req, res, next) {
     res.donor = donor; // Now, res.donor is a MongoDB document object
     next();
   }
+
+  exports.api = functions.https.onRequest(app)
